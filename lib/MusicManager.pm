a4778aba2f7163b5b3f4edd9a3bc9a8f20b06161
Brad Lhotsky <brad.lhotsky@gmail.com>
2012-04-09 19:34:56 +0200

    more stuff added

--- lib/MusicManager.pm
+++ lib/MusicManager.pm
@@ -2,7 +2,7 @@ package MusicManager;
 use Mojo::Base 'Mojolicious';
 use Audio::MPD;
 
-has 'mpd' => sub { Audio::MPD->new( { host => '10.0.1.10' } ); };
+has 'mpd' => sub { my $self = shift; return Audio::MPD->new( { host => $self->config->{mpd}{host} } ); };
 
 # This method will run once at server start
 sub startup {
@@ -12,13 +12,20 @@ sub startup {
     $self->secret('my mpd secret is sooo cool!');
     $self->sessions->default_expiration(3600*24*7);
 
+    # Configuration
+    my $config = $self->plugin( yaml_config => {
+            file => 'music_manager.yaml',
+            stash_key   => 'config',
+    });
+
     # Hook to refresh current_track
     $self->hook( before_dispatch => sub {
-              $self->defaults( current_track => $self->mpd->current )
+              my $c = shift;
+              $c->stash( current_track => $c->app->mpd->current )
           }
     );
 
-    # Router
+    # Routing via Controller::*
     my $r = $self->routes;
     $r->namespace('MusicManager::Controller');
 
--- lib/MusicManager/Controller/Main.pm
+++ lib/MusicManager/Controller/Main.pm
@@ -4,7 +4,6 @@ use Mojo::Base 'Mojolicious::Controller';
 sub index {
   my $self = shift;
 
-  $self->stash( current_track => $self->app->mpd->current );
   $self->render();
 }
 
