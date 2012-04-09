c76312ee69a1e25413060263369d8e3cd1893803
Brad Lhotsky <brad.lhotsky@gmail.com>
2012-04-09 16:44:46 +0200

    Initial Import

---
+++ lib/MusicManager.pm
@@ -0,0 +1,15 @@
+package MusicManager;
+use Mojo::Base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+  my $self = shift;
+
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->route('/')->to('main#index');
+}
+
+1;
