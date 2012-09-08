df7561b73419788fcc62a2bc8d44c58fc339dcea
Brad Lhotsky <brad.lhotsky@gmail.com>
2012-09-08 22:31:50 +0200

    Starting the search module

--- lib/MusicManager/Controller/Search.pm
+++ lib/MusicManager/Controller/Search.pm
@@ -3,6 +3,8 @@ use Mojo::Base 'Mojolicious::Controller';
 
 sub songs {
     my $self = shift;
+    my $qs = $self->param('qs');
+
     $self->render();
 }
 
--- lib/MusicManager/Model/Files.pm
+++ lib/MusicManager/Model/Files.pm
@@ -29,3 +29,26 @@ sub song_at {
     my $safe_path = File::Spec->catfile( @safe );
 
 }
+
+sub song_search {
+    my ($self,@keywords) = @_;
+
+    # Build search strings
+    my @search = ();
+    my @files = map { "*.$_" } keys %_valid_media_exts;
+    foreach my $token ( @keywords  ) {
+        $token =~ s/[^A-Za-z0-9']//g;
+        push @search, qr/$token/;
+    }
+
+    my $dir = File::Spec->catdir( $self->media_dir );
+    my $STRIP_MEDIA_DIR = 1 + length $self->media_dir;
+
+    # Do the search
+    my $rule = File::Find::Rule->new();
+    my @matches = map { substr $_, $STRIP_MEDIA_DIR } $rule->file()->in( $dir )->or(
+        $rule->new->file->and( $rule->new->name( @search ), $rule->new->name(@files) ),
+        $rule->new->file->and( $rule->new->directory->name( @search ), $rule->new->name(@files) )
+    );
+
+}
